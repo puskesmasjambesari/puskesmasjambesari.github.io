18a428c4482e1d62513aee31ddde448dbe229112
puskesmasjambesari <sip.puskesmas.jambesari@gmail.com>
2026-08-15 23:18:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ const WORKER_URL = "https://gemini-proxy.sip-puskesmas-jambesari.workers.dev";
 // GANTI DENGAN URL RAW FILE REGULASI DI GITHUB ANDA 
 const REGULASI_URLS = [
     "regulasi/KMK Nomor 2015 Tahun 2023 tentang Petunjuk Teknis Integrasi Pelayanan Kesehatan Primer.txt",
-    "regulasi/Pedoman Teknis Pencegahan dan Pengendalian Infeksi di Fasilitas Pelayanan Kesehatan Tingkat Pertama",
-    "regulasi/Permenkes No 11 Tahun 2017 Tentang Keselamatan Pasien",
-    "regulasi/permenkes-no-19-tahun-2024"
+    "regulasi/Pedoman Teknis Pencegahan dan Pengendalian Infeksi di Fasilitas Pelayanan Kesehatan Tingkat Pertama.txt",
+    "regulasi/Permenkes No 11 Tahun 2017 Tentang Keselamatan Pasien.txt",
+    "regulasi/permenkes no 19 tahun 2024.txt"
     // Tambahkan URL lain di sini, pisahkan dengan koma
 ];
 
